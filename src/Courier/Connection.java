e12668717b35806b012cc3396b7e2ada590d8545
omer <omer@omer-PC>
2016-01-17 17:42:39 +0200

    new commit

---
+++ src/Courier/Connection.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Courier;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+/**
+ *
+ * @author omer
+ */
+class Connection {
+
+    PreparedStatement prepareStatement(String select__from_login_where_username_and_pas) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Statement createStatemen() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/java.java
@@ -0,0 +1,10 @@
+package Admin;
+
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.B350AA1E-E7FE-04CF-B8BC-07CD31BC1F1C]
+// </editor-fold> 
+public class java {
+
+}
+
